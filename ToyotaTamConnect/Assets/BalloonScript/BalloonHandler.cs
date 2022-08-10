7a4aa5916c604a9bf25ed736d540641974614a9c
Farhan Muhammad <farhnmh@gmail.com>
2022-08-10 13:34:09 +0700

    Upload Initiation

---
+++ ToyotaTamConnect/Assets/BalloonScript/BalloonHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonHandler : MonoBehaviour
+{
+    public Animator animator;
+    public float flySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * flySpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Question"))
+            animator.SetTrigger("isPopOut");
+    }
+}
