92ebe052f82b41e8c5fb6e032107c666aac9f3d1
Farhan Muhammad <farhnmh@gmail.com>
2022-08-15 14:40:43 +0700

    Update

--- ToyotaTamConnect/Assets/BalloonScript/BalloonHandler.cs
+++ ToyotaTamConnect/Assets/BalloonScript/BalloonHandler.cs
@@ -7,21 +7,27 @@ public class BalloonHandler : MonoBehaviour
     public Animator animator;
     public float flySpeed;
 
+    Vector3 lastVelocity;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(20 * Time.deltaTime * flySpeed, 
+                                20 * Time.deltaTime * flySpeed));
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * flySpeed * Time.deltaTime;
+        lastVelocity = rb.velocity;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Question"))
-            animator.SetTrigger("isPopOut");
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+        rb.velocity = direction * Mathf.Max(speed, 0f);
     }
 }
