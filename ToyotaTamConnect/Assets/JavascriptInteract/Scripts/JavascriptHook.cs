adc9a2036176f1a68305789e5693d8a667038f97
Farhan Muhammad <farhnmh@gmail.com>
2022-08-16 15:41:41 +0700

    Set n Get Data On Local Storage

---
+++ ToyotaTamConnect/Assets/JavascriptInteract/Scripts/JavascriptHook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class PlayerData
+{
+    public string email;
+    public string ticket;
+}
+
+public class JavascriptHook : MonoBehaviour
+{
+    public TextMeshProUGUI detailText;
+    public PlayerData playerData;
+
+    void Update()
+    {
+        detailText.text = $"{playerData.email}\n{playerData.ticket}";
+    }
+
+    public void PlayerEmailHandler(string email) {
+        playerData.email = email;
+    }
+
+    public void PlayerTicketHandler(string ticket)
+    {
+        playerData.ticket = ticket;
+    }
+}
---
+++ ToyotaTamConnect/Assets/SpriteChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] newSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
