aee8a94fe4ad78f9cfa6f26b512460417ebcb9fc
Frederiko Adrian <frederiko.adrian@gmail.com>
2022-08-18 20:10:35 +0700

    Video w/ streaminassets

--- ToyotaTamConnect/Assets/Script/VideoScript.cs
+++ ToyotaTamConnect/Assets/Script/VideoScript.cs
@@ -5,13 +5,14 @@ using UnityEngine.Video;
 
 public class VideoScript : MonoBehaviour
 {
-    //[SerializeField] string url;
+    [SerializeField] string videoClipName;
     [SerializeField] VideoPlayer videoPlayer;
     [SerializeField] GameObject doneButton;
     // Start is called before the first frame update
     private void Awake()
     {
-        //videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, url);
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, videoClipName);
+        StartCoroutine(TryPlayVideo());
     }
     void Start()
     {
@@ -20,5 +21,14 @@ public class VideoScript : MonoBehaviour
     void EndReached(UnityEngine.Video.VideoPlayer vp)
     {
         doneButton.SetActive(true);
+    }
+    private IEnumerator TryPlayVideo()
+    {
+        Debug.Log("Trying to play video");
+        while (!videoPlayer.isPlaying)
+        {
+            videoPlayer.Play();
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
